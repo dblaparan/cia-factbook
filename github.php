2159d0cdb5d0083191ef2a8cec7f92e9e34c57c5
dblaparan <dblaparan@up.edu.ph>
2025-10-18 15:05:23 +0000

    first commit

---
+++ github.php
@@ -0,0 +1,13 @@
+<?php
+$secret = trim(@file_get_contents('/var/www/github.secret'));
+
+$header = 'HTTP_X_HUB_SIGNATURE';
+$payload = file_get_contents('php://input');
+$hash = 'sha1=' . hash_hmac('sha1', $payload, $secret);
+
+if (isset($_SERVER[$header]) && $_SERVER[$header] === $hash) {
+    $payload_decoded = json_decode($payload, true);
+    if ($payload_decoded['ref'] === 'refs/heads/main') {
+        shell_exec('git reset --hard HEAD && git fetch origin main && git pull origin main');
+    }
+}
